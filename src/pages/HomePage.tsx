fc53656787276dbcb6c846904e7d7045033afb78
Jordan Yamada <sadaotomokane@gmail.com>
2024-05-02 17:39:47 +0000

    added conditional rendering for the homepage for whether the user id logged in

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useOutletContext } from 'react-router-dom';
+import { User } from '../utilities/utilities';
+
+
+interface OutletContext {
+  user: User;
+}
 
 
 const HomePage: React.FC = () => {
+
+  const { user }: OutletContext = useOutletContext();
 
 
   return (
     <>
       <h1>Welcome</h1>
-      <br/>
-      <div>
-        Embark on an...
-      <Link to="/adventure/">Adventure</Link>
-      </div>
-      <br/>
-      <div>
-        Browse through the...
-      <Link to="/library/">Library</Link>
-      </div>
-      <br/>
-      <div>
-        Delve through your...
-      <Link to="/memories/">Memories</Link>
-      </div>
+      {user
+        ?
+        <>
+          <br />
+          <div>
+            Embark on an...
+            <Link to="/adventure/">Adventure</Link>
+          </div>
+          <br />
+          <div>
+            Browse through the...
+            <Link to="/library/">Library</Link>
+          </div>
+          <br />
+          <div>
+            Delve through your...
+            <Link to="/memories/">Memories</Link>
+          </div>
+        </>
+        :
+        <>
+          <br />
+          <h2>Please <Link to="/signup/">Sign Up</Link> or <Link to="/login/">Log In</Link> to Continue</h2>
+        </>
+      }
     </>
   )
 }
